23532ab547f790eaa5b96d5a3d393352ebbd4cd6
uziel <silyeze5@gmail.com>
2021-05-25 20:29:50 -0300

    commit8

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,4 +1,4 @@
-document.getElementById("btn").addEventListener("click","touch", e=>{
+document.getElementById("btn").addEventListener("click", e=>{
    let keyword = document.getElementById("search").value;
    Giphy.getUrlAsync(keyword,function(videoUrl){
      document.getElementById("gif").src = videoUrl;
