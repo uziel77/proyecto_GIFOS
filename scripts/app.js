1d13a948a598a2502fc43bc6836be65d79470761
uziel <silyeze5@gmail.com>
2021-05-26 17:34:32 -0300

    commit7

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,26 @@
-class Giphy{
-    constructor(keyword){
-      this.keyword = keyword;
-      this.endpoint = "http://api.giphy.com/v1/gifs"
-      this.api_key = "k7myyVYXWc9zebI6Yrrm5zPMspeexlxV";
+
+   function getGifUrl(keyword){
+      return fetch("http://api.giphy.com/v1/gifs/search?api_key=k7myyVYXWc9zebI6Yrrm5zPMspeexlxV&q="+keyword+"&limit=12").then(response => response.json());
     }
-    getGifUrl(callback){
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET",this.endpoint+"/translate?api_key="+this.api_key+"&s="+this.keyword);
-        xhr.responseType = "json";
-        xhr.onload = function(){
-            callback(this.response.data.images.original.mp4)
-        }
-        xhr.send();
+    async function llamadaGif(keyword) {
+        const result = await this.getGifUrl(keyword);
+        let promesa = [];
+        result.data.forEach(gif => {
+            let imagen = new Image();
+            imagen.src = gif.images.original.url;
+            promesa.push(cargarImagen(imagen));
+            agregarImagen(imagen);
+         });
+        await Promise.all(promesa)
     }
-    static getUrlAsync(keyword,callback){
-        return new Giphy(keyword).getGifUrl(callback);
+    function cargarImagen(imagen){
+       return new Promise(resolve => imagen.onload = resolve);
     }
+    function agregarImagen(imagen){
+        let div = document.createElement('div');
+        div.className = 'gifos';
+        div.appendChild(imagen);
+        let divImg = document.getElementById("gifs");
+        divImg.appendChild(div);
 }
+
--- scripts/app2.js
+++ scripts/app2.js
@@ -1,9 +1,11 @@
 document.getElementById("btn").addEventListener("click", e=>{
    let keyword = document.getElementById("search").value;
-   Giphy.getUrlAsync(keyword,function(videoUrl){
-     document.getElementById("gif").src = videoUrl;
-   })
+   llamadaGif(keyword);
 })
+
+
+
+
 
 
 function modoOscuro() {
